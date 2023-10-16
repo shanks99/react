63bf96c9b7887b36e326b0e722a101352e125fec
Shanks999 <j1004sup@gmail.com>
2023-10-17 00:04:20 +0900

    Route and About

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
+import {Routes, Route, Link} from "react-router-dom" // Routes, Route, Link 사용
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* Link는 '<a>' 개념. to는 'href' Attr
+      */}
+      <nav>
+      <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+
+      {/* Link의 to를 => Route의 path와 연결 */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
